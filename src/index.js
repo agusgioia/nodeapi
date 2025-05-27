6e0f53d9f5c79b578718075256b07f349d52cc91
agusgioia <agusgioia@gmail.com>
2025-05-27 17:00:54 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const personajeRoutes = require('./src/routes/personajeRoutes');
+const personajeRoutes = require('./routes/personajeRoutes');
 require('dotenv').config();
 
 const app = express();
