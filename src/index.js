076ea8ed898e736c0e9d6042bbff375690e2c02a
agusgioia <agusgioia@gmail.com>
2025-05-27 20:28:29 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const personajeRoutes = require('./routes/personajeRoutes');
 require('dotenv').config();
-
+const cors = require('cors');
 const app = express();
 const port = process.env.APP_PORT;
 
 app.use(bodyParser.json());
-
+app.use(cors({
+    origin:'',
+    methods:['GET','POST','PUT','PATCH','DELETE'],
+    credentials:true
+}));
 // Rutas
 app.use('/api/personajes', personajeRoutes);
 
