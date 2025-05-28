6c36c338e0b16f5e3f3a33becb6ee899ad9c8f97
agusgioia <agusgioia@gmail.com>
2025-05-28 12:48:12 -0300

    rutas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const personajeRoutes = require('./routes/personajeRoutes');
+const personajeRoutes = require('./routes/routes');
+const userRoutes = require('./routes/routes')
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -8,12 +9,13 @@ const port = process.env.APP_PORT;
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin:'',
+    origin:'htpp://localhost:5173',
     methods:['GET','POST','PUT','PATCH','DELETE'],
     credentials:true
 }));
 // Rutas
 app.use('/api/personajes', personajeRoutes);
+app.use('/api/users',userRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
 const personajeController = require('../controllers/personajeController');
-const userController = require('../controllers/userController');
 
 router.get('/', personajeController.getPersonajes);
 router.get('/:id',personajeController.getPersonajeById);
@@ -9,6 +9,8 @@ router.post('/',personajeController.postPersonaje);
 router.put('/:id',personajeController.putPersonaje);
 router.delete('/:id',personajeController.deletePersonaje);
 
+
+const userController = require('../controllers/userController');
 router.get('/', userController.getUsers);
 router.get('/:id',userController.getUserById);
 router.post('/',userController.postUser);
